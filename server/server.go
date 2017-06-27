3d53f2ccb419829ae190d77236704a892acdd473
Daisuke Fujita <dtanshi45@gmail.com>
2017-06-27 15:30:13 +0900

    Refactor middleware code in server

--- server/server.go
+++ server/server.go
@@ -4,24 +4,23 @@ import (
 	"fmt"
 
 	"github.com/dtan4/imageup/docker"
-	imageupMW "github.com/dtan4/imageup/server/middleware"
+	"github.com/dtan4/imageup/server/middleware"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	echoMW "github.com/labstack/echo/middleware"
 )
 
 // Run starts ImageUp server
 func Run(port int) {
 	e := echo.New()
 
+	e.Use(echoMW.Logger())
+	e.Use(echoMW.Recover())
+
 	dockerClient, err := docker.NewClient()
 	if err != nil {
 		e.Logger.Fatal(err)
 	}
-
-	e.Use(imageupMW.SetDockerClient(dockerClient))
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	e.Use(middleware.SetDockerClient(dockerClient))
 
 	drawRoutes(e)
 
