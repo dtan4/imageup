7f4cf29678df8725bf6d5200890fc874cdee2772
Daisuke Fujita <dtanshi45@gmail.com>
2017-06-28 18:46:37 +0900

    Enable basic auth if specified

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"fmt"
 
+	"github.com/dtan4/imageup/config"
 	"github.com/dtan4/imageup/docker"
 	"github.com/dtan4/imageup/server/middleware"
 	"github.com/labstack/echo"
@@ -11,11 +12,17 @@ import (
 )
 
 // Run starts ImageUp server
-func Run(port int) {
+func Run(conf *config.Config) {
 	e := echo.New()
 
 	e.Use(echoMW.Logger())
 	e.Use(echoMW.Recover())
+
+	if conf.BasicAuthUsername != "" && conf.BasicAuthPassword != "" {
+		e.Use(echoMW.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+			return username == conf.BasicAuthUsername && password == conf.BasicAuthPassword, nil
+		}))
+	}
 
 	dockerClient, err := docker.NewClient()
 	if err != nil {
@@ -25,7 +32,7 @@ func Run(port int) {
 
 	drawRoutes(e)
 
-	addr := fmt.Sprintf(":%d", port)
+	addr := fmt.Sprintf(":%d", conf.Port)
 
 	e.Logger.SetLevel(log.ERROR)
 	e.Logger.Fatal(e.Start(addr))
