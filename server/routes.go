9121e7f0b0290dad4d91ac8a3b6ccd31134c614b
Daisuke Fujita <dtanshi45@gmail.com>
2017-07-06 17:19:19 +0900

    Migrate to handler package

--- server/routes.go
+++ server/routes.go
@@ -2,22 +2,23 @@ package server
 
 import (
 	"github.com/dtan4/imageup/config"
+	"github.com/dtan4/imageup/server/handler"
 	"github.com/labstack/echo"
 	echoMW "github.com/labstack/echo/middleware"
 )
 
 func drawRoutes(e *echo.Echo, conf *config.Config) {
-	e.GET("/ping", pingHandler)
+	e.GET("/ping", handler.PingHandler)
 
 	if conf.BasicAuthUsername != "" && conf.BasicAuthPassword != "" {
 		basicAuth := echoMW.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
 			return username == conf.BasicAuthUsername && password == conf.BasicAuthPassword, nil
 		})
 
-		e.GET("/", rootHandler, basicAuth)
-		e.POST("/webhooks/quay", webhooksQuayHandler, basicAuth)
+		e.GET("/", handler.RootHandler, basicAuth)
+		e.POST("/webhooks/quay", handler.WebhooksQuayHandler, basicAuth)
 	} else {
-		e.GET("/", rootHandler)
-		e.POST("/webhooks/quay", webhooksQuayHandler)
+		e.GET("/", handler.RootHandler)
+		e.POST("/webhooks/quay", handler.WebhooksQuayHandler)
 	}
 }
