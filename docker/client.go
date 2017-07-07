9ee18f1b13e7a2a5764b0f2d8db79004d3445cf6
Daisuke Fujita <dtanshi45@gmail.com>
2017-07-07 12:26:02 +0900

    Pull private images in Quay.io

--- docker/client.go
+++ docker/client.go
@@ -2,10 +2,15 @@ package docker
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/cli/config"
+	"github.com/docker/docker/cli/config/configfile"
+	"github.com/docker/docker/cli/config/credentials"
 	"github.com/docker/docker/client"
 	"github.com/pkg/errors"
 )
@@ -18,6 +23,7 @@ type Client interface {
 // RealClient represents the wrapper of Docker API client
 type RealClient struct {
 	cli *client.Client
+	cfg *configfile.ConfigFile
 }
 
 // NewClient creates new RealClient object
@@ -27,8 +33,14 @@ func NewClient() (*RealClient, error) {
 		return nil, errors.Wrap(err, "cannot create Docker Engine API client")
 	}
 
+	cfg, err := config.Load("")
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot load docker config")
+	}
+
 	return &RealClient{
 		cli: cli,
+		cfg: cfg,
 	}, nil
 }
 
@@ -36,7 +48,23 @@ func NewClient() (*RealClient, error) {
 func (c *RealClient) PullImage(image, tag string) (io.ReadCloser, error) {
 	imageRef := fmt.Sprintf("%s:%s", image, tag)
 
-	out, err := c.cli.ImagePull(context.Background(), imageRef, types.ImagePullOptions{})
+	store := credentials.NewNativeStore(c.cfg, c.cfg.CredentialsStore)
+
+	newAuths, err := store.GetAll()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get Docker credentials")
+	}
+
+	buf, err := json.Marshal(newAuths["https://quay.io"])
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal authConfig to JSON")
+	}
+
+	fmt.Println(string(buf))
+
+	out, err := c.cli.ImagePull(context.Background(), imageRef, types.ImagePullOptions{
+		RegistryAuth: base64.URLEncoding.EncodeToString(buf),
+	})
 	if err != nil {
 		return nil, errors.Wrapf(err, "cannot pull image %q", imageRef)
 	}
